f092eddf52495ccc54879e5a73f635a28436fb81
Kevin <adamsk34@myumanitoba.ca>
2020-03-07 17:36:53 -0600

    Servo moves at all times

--- Source/main.c
+++ Source/main.c
@@ -30,6 +30,11 @@ void nextCoffeeType(void);
 void initDebounceTimer(void);
 void enableDebounceInterrupt(void);
 
+void setSysTick(void);
+void InitServos (void);
+void InitPWMTimer4(void);
+void SetupPWM(void);
+
 enum states {
 	cyclingCoffeeTypes
 };
@@ -63,6 +68,8 @@ int unpressButtonOccurred = 0;// false
 int dpDisableSinglePress = 0;// false
 // long press disable single press
 int lpDisableSinglePress = 0;// false
+
+//volatile uint32_t msTicks;// Counts 1ms timeTicks
 
 TickType_t ticksLastPress = NULL;
 TickType_t ticksLastUnpress = NULL;
@@ -211,7 +218,20 @@ void vShowCoffeeSelected(void *pvParameters) {
 }
 
 void vIdle(void *pvParameters) {
-	while(1);
+	setSysTick();
+  InitServos();
+  InitPWMTimer4();
+  SetupPWM();
+	
+	while(1) {
+		//only control one servo at timer 4, channel 1 
+    TIM4->CCR1 = 600;    // 0.6 ms
+    vTaskDelay(1000);
+    TIM4->CCR1 = 1500; // 1.5 ms
+    vTaskDelay(1000);
+    TIM4->CCR1 = 2200; // 2.2 ms
+    vTaskDelay(1000);
+	}
 }
 
 // ************************************** Button Events **************************************
@@ -225,9 +245,8 @@ void doublePressButtonEvent() {
 // long press (not single press, not double press)
 // only called once for each long press
 void longPressButtonEvent() {
-	STM_EVAL_LEDToggle(LED_GREEN);
+	
 }
-
 
 // single press (not double press, not long press)
 // only called once for each single press
@@ -251,6 +270,58 @@ void pressButtonEvent() {
 // only called once for each unpress
 void unpressButtonEvent() {
 	
+}
+
+// ************************************** Servos **************************************
+
+void setSysTick(void){
+    if (SysTick_Config(SystemCoreClock / 1000)) {
+        while (1){};
+    }
+}
+
+void InitServos (void){ 
+    GPIO_InitTypeDef GPIO_InitStructure;
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+    //Initialize PB6 (TIM4 Ch1) and PB7 (TIM4 Ch2)
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;    // GPIO_High_Speed
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+    // Assign Alternate Functions to pins
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_TIM4);
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_TIM4);
+}
+
+void InitPWMTimer4(void) {
+  TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+  //TIM4 Clock Enable
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+  // Time Base Configuration for 50Hz
+  TIM_TimeBaseStructure.TIM_Period = 20000 - 1;
+  TIM_TimeBaseStructure.TIM_Prescaler = 84 -1;
+  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+  TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
+  TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
+  TIM_Cmd(TIM4, ENABLE);
+}
+
+void SetupPWM(void) {
+    
+  TIM_OCInitTypeDef TIM_OCInitStructure;
+  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1; //Set output capture as PWM mode
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable; //Enable output compare
+  TIM_OCInitStructure.TIM_Pulse = 0; // Initial duty cycle at 0%
+  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High; // HIGH output compare active
+  // Set the output capture channel 1 and 2 (upto 4)
+  TIM_OC1Init(TIM4, &TIM_OCInitStructure); // Channel 1
+  TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
+  TIM_OC2Init(TIM4, &TIM_OCInitStructure); // Channel 2
+  TIM_OC2PreloadConfig(TIM4, TIM_OCPreload_Enable);
+  TIM_ARRPreloadConfig(TIM4, ENABLE);
 }
 
 // ************************************** Timers **************************************
